26bf797e67cef571f10b95a119c256361a858467
Shivam Kumar <shivamkur22@iitk.ac.in>
2024-02-15 18:22:19 +0530

    Updated FAQ

--- js/faq.js
+++ js/faq.js
@@ -1,11 +1,11 @@
 const faq = [
   {
-    title: "What is Ajay and Rooma Dubey HII program?",
+    title: "What is Rooma & Ajay Dubey’s HII Program?",
     content:
-      "It is a program for supporting the students of IITK in the field of Healthtech innovation to convert their ideas into reality. Through this program, such talented students will get a chance to prove their talent and convert their project ideas to develop product design prototypes.",
+      "It is a program for supporting the students of IITK in the field of Health-care innovation to convert their ideas into reality. Through this program, such talented students will get a chance to prove their talent and convert their project ideas to develop product design prototypes.",
   },
   {
-    title: "Who is Ajay Dubey?",
+    title: "Who is Mr. Ajay Dubey?",
     content:
       "He graduated from IIT Kanpur in the year 1980 and later held various leadership positions in the technology industry. He served as the Vice President at Infosys, leading the Pune Development Center, and also worked as the COO at Persistent Systems Pvt. Ltd. He is an Angel Investor at Clodura Systems.",
   },
@@ -29,22 +29,18 @@ const faq = [
   {
     title: "What is the timeline for the program?",
     content:
-      "Application Start Date: <strong>05/03/2023</strong>, <br/> Application Close Date: <strong>20/03/2023</strong>, <br/> Result Announcement Date: <strong>06/04/2023</strong>",
+      "Application Start Date: <strong>February 15, 2024</strong>, <br/> Application Close Date: <strong>March 15, 2024</strong>",
   },
   {
     title: "Who is eligible to participate in the program?",
     content: `<ul>
-        <li>This program will be open to all UG students who have completed their 4th semester and are yet to begin their 5th semester</li>
-        <li>Dual Degree students who completed the 6th  Semester will be also eligible</li>
-        <li>Students from all the departments</li>
-        <li>A team of 3-4 students coming from at least two different departments or an Interdisciplinary team of 3-4 students</li>
-        <li>No merit criteria are needed or Student’s CGPGI shall not be included in the evaluation criteria</li>
+    This program will be open to all UG (2nd year onwards), PG(1st sem onwards), PhD students who have completed.
       </ul>`,
   },
   {
     title: "How can I apply for the program?",
     content:
-      "Through the application link attached to the website. To apply to this program please click<a style='display:inline' href='https://docs.google.com/forms/d/e/1FAIpQLSdKSpVPIVEFgST6q18eIQDZLZalCLQdvD7qKo9t-FJFe22zWw/viewform'>here</a>",
+      "Through the application link attached to the website. To apply to this program please click<a style='display:inline' href='https://forms.gle/2B32XMtqjLBdwoxC7'>here</a>",
   },
   {
     title: "Are there any application costs associated with participating?",
@@ -53,20 +49,23 @@ const faq = [
   {
     title: "What are the benefits of participating in the program?",
     content: `
-    A chance to immerse in hospitals and medical industries for closely understanding the pain points and identifying challenges in the Indian healthcare system. <br/>
-    An opportunity to disrupt the Indian healthcare system with innovative technological solutions and impact millions of Indian lives. <br/>
-    A structured program to experience and train oneself for entrepreneurship in healthcare. <br/>
-    Through field exposure (Summer Internship Program) a chance to identify relevant engineering problems in healthcare to solve. <br/>
-    Innovators/ startups are connected with viable funding and networking opportunities to fast-track their growth. Helps create and nurture relationships with peers, encourages team work, develops problem-solving skills and facilitate connection with an expert, professional network. <br/>
-    Get access to an amazing community of business leaders, investors, managers with proven experience, and faculty at IIT Kanpur offering unmatched technical and business expertise. <br/>
-    Have access to world-class infrastructure at IIT Kanpur at faculty/student rates. <br/>
-    Provide founders access to multiple funding pipelines throughout their startup journey. Programs offer mentorship and technical support across ideation, prototype, and seed stage. Offer some major schemes like SPARSH, BIG, TIDE 2.0, NIDHI EiR, and IAIN across domains.
+    A chance to immerse in hospitals and medical industries for closely understanding the pain points and identifying challenges in the Indian healthcare system.
+ <br/>
+ Once in a lifetime opportunity to disrupt the Indian healthcare system with innovative technological solutions and impact millions of Indian lives. <br/>
+ A structured program to experience and train oneself for entrepreneurship in healthcare. <br/>
+ Through field exposure (immersion program) a chance to identify relevant
+ engineering problems in healthcare to solve.
+  <br/>
+  Innovators/ startups are connected with viable funding and networking opportunities to fast-track their growth. Help founders nurture relationships with peers, and facilitate connection with an expert, professional network. <br/>
+  Get access to an amazing community of business leaders, investors, managers with proven experience, and faculty at IIT Kanpur offering unmatched technical and business expertise. <br/>
+  Have access to world-class infrastructure at IIT Kanpur at faculty/student rates. <br/>
+  Provide founders access to multiple funding pipelines throughout their startup journey. Programs offer mentorship and technical support across ideation, prototype, and seed stage. Offer some major schemes like SPARSH, BIG, TIDE 2.0, NIDHI EiR, and IAIN across domains.
     `,
   },
   {
     title: "What is the selection process ( will be conducted by SIIC)?",
     content:
-      "<strong>Round 1:</strong> Shortlisting of applications based on the response and the eligibility criteria, <br/> <Strong>Round 2:</Strong> First round of interview, <br/> <strong>Round 3:</strong> The selected students from Round 2 shall be given case studies to read and review and make a presentation, <br/> Based on the presentation by the selected student groups and their application. The final list of student groups will be selected.",
+      "<strong>Round 1:</strong> Shortlisting of applications based on the response and the eligibility criteria., <br/> <Strong>Round 2:</Strong> First round of interview, <br/> <strong>Round 3:</strong> The selected students from Round 2 shall be given case studies to read and review and make a presentation., <br/> Based on the presentation by the selected student groups and their application. The final list of student groups will be selected.",
   },
   {
     title: "Who will be the selection committee members?",
@@ -81,21 +80,21 @@ const faq = [
   {
     title: "What all costs will be covered?",
     content:
-      "Travel, Accommodation, food and any other miscellaneous expenses required to complete the Summer Internship.",
+      "Travel, Accommodation, Food expenses and any other miscellaneous expenses required to complete the Summer Internship.",
   },
   {
     title:
       "How much will I receive for all the expense heads mentioned in the previous question?",
-    content: "It will be decided once the teams are finalized.",
+    content: "It will be decided once the students are selected.",
   },
   {
     title: "Who will be managing these all expenses?",
-    content: "SIIC",
+    content: "The generous contribution of Mr. Dubey will cover all the expenses for the program.",
   },
   {
     title:
       "Can the student participate alone or with a group outside the IITK?",
-    content: "No",
+    content: "Unfortunately, the program supports only students from the institute and no outsiders would be able to participate directly on indirectly.",
   },
   {
     title:
@@ -103,18 +102,12 @@ const faq = [
     content: "Yes",
   },
   {
-    title:
-      "What if I do not participate to find a problem statement and prototype development?",
-    content:
-      "In this case, the expenses incurred will be recovered from the student group.",
-  },
-  {
     title: "How many students can take part in a group?",
     content: "3 to 4 students",
   },
   {
     title: "Is there any mandate to incubate in SIIC?",
-    content: "Yes. The student group sign a binding agreement for this",
+    content: "Yes. The student group sign a binding agreement for this.",
   },
   {
     title: "Do student groups need to sign any agreement?",
@@ -133,13 +126,24 @@ const faq = [
   {
     title: "What after prototype development?",
     content:
-      "In the 5th and 6th semesters, the SIIC will provide assistance post-incubation to improve upon the prototype and plan for manufacturing as well as commercialization.",
+      "The program goals to give an entrepreneurship exposure to the students after they have completed the innovation journey.",
   },
   {
     title: "POC from E Cell, IITK DF and SIIC?",
     content:
-      "<strong>E Cell</strong> - Vinit Toshniwal <strong>vinit@ecelliitk.com</strong>, <br/> <strong>SIIC</strong> - Aditi Kumar <strong>aditikr@iitk.ac.in</strong>, <br/> <strong>IITKDF</strong> - Mradul Verma <strong>programs_dora@iitk.ac.in</strong>",
+      "<strong>E Cell</strong> - Piyush Raj <strong>rpiyush21@iitk.ac.in</strong>, <br/> <strong>SIIC</strong> - Aditi Kumar <strong>aditikr@iitk.ac.in</strong>, <br/> <strong>IITKDF</strong> - Mradul Verma <strong>programs_dora@iitk.ac.in</strong>",
   },
+  {
+    title: "What is Clinical immersion?",
+    content:
+      "One of the part of the program is clinical immersion in which the fellows get to visit Clinical institutions, learn about the technology and healthcare system of the country and to find problem statements that they should pursue during the course of this program.",
+  },
+  {
+    title: "Where did the students of the previous Cohort go for the clinical immersions?",
+    content:
+      "The fellows of the last cohort went to Sree Chitra Tirunal Institute of Medical Sciences and Technology, Trivandrum, Kerela.",
+  },
+  
 ];
 
 let opened = -1;
